06a39ff44edbb8f158b104746d73fe24275c02b9
stas <79771234+ninahur1989@users.noreply.github.com>
2022-09-08 14:14:55 +0300

    added authorization and authentication

---
+++ Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace Module6HW5.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public int? RoleId { get; set; }
+
+        public Role Role { get; set; }
+    }
+}
